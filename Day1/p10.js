d1cafd3b4993245306c6ee6c83c8fff98167b8a3
Vidushi Rathore <sumatirathore038@gmail.come>
2025-02-07 12:18:13 +0530

    message

---
+++ Day1/p10.js
@@ -0,0 +1,4 @@
+let sum=(a,b)=>{return a+b}
+let mul=(a,b)=>{return a*b}
+let diff=(a,b)=>{return a-b}
+module.exports={sum,mul,diff};
---
+++ Day1/p11.js
@@ -0,0 +1,4 @@
+const{sum,mul,diff}=require('./p10.js');
+console.log(sum(5,10));
+console.log(mul(20,30));
+console.log(diff(30,10));
